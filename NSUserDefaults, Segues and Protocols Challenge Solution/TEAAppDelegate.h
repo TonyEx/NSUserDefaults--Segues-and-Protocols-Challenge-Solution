ebe7e8756a7310565e7b4dbbe8882183640ad740
TonyEx <tonyangelox@veronica.home>
2014-07-27 18:04:54 -0400

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Tony Angelo on 7/27/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Tony Angelo on 7/27/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
