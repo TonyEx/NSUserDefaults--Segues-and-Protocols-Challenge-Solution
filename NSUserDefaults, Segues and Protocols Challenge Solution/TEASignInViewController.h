5288e4c4b4528dd8379dbba5f9a36358bc80d002
TonyEx <tonyangelox@veronica.home>
2014-07-27 18:15:17 -0400

    Initial Setup.

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/TEASignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEASignInViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Tony Angelo on 7/27/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEASignInViewController : UIViewController
+
+@end
